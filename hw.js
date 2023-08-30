5ba8bedd06113ce07637531c2fcb3de68ab4c34f
EdenPrime92 <132381824+EdenPrime92@users.noreply.github.com>
2023-08-30 19:37:20 +0300

    Update hw.js

--- hw.js
+++ hw.js
@@ -1,30 +1,31 @@
 //closure bucket
 
-function measureOutFourLiters () {
+function measureOutFourLiters() {
     let bucket3L = 0
     let bucket5L = 0
-    
-    function fill5LBucket(){
-        bucket5L = 5
+
+    function fill5LBucket() {
+        bucket5L += 5
     }
 
-    function pourWaterInBuckets (){
-        bucket5L -= 3
-        bucket3L += 3
+    function pourWaterInBuckets() {
+        bucket3L = bucket5L - 3
     }
 
-    function empty3LBucket () {
+    function empty3LBucket() {
         bucket3L -= 3
     }
 
-    function pour2Lto3LBucket (){
-        bucket5L -= 2
-        bucket3L += 2
+    function pour2Lto3LBucket() {
+        bucket3L = bucket5L - 2
     }
 
-    function pour1Lto3LBucket () {
-        bucket5L -= 1
-        bucket3L += 1
+    function pour1Lto3LBucket() {
+        bucket3L = bucket5L - 4
+    }
+
+    function restOftheWaterIn5LBucket() {
+        bucket5L = bucket5L - bucket3L
     }
 
     fill5LBucket()
@@ -33,10 +34,14 @@ function measureOutFourLiters () {
     pour2Lto3LBucket()
     fill5LBucket()
     pour1Lto3LBucket()
+    restOftheWaterIn5LBucket()
 
     return bucket5L
 }
 
 const result = measureOutFourLiters()
 console.log("Остаток воды в 5 литровом ведре :" + " " + result + " " + "литра")
+
+
+
 
