8bbedb8d1483870493e3b1c02c3ee95038767966
EdenPrime92 <132381824+EdenPrime92@users.noreply.github.com>
2023-09-17 18:30:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
+function createDivElements(arr) {
+    const filteredUsers = arr.filter(user => !user.first_name);
+    filteredUsers.forEach(user => {
+        const userDiv = document.createElement("div");
+        userDiv.className = "user";
+
+        const avatarImg = document.createElement("img");
+        avatarImg.src = user.avatar_url;
+        avatarImg.alt = "Avatar";
+
+        const userIdDiv = document.createElement("div");
+        userIdDiv.textContent = `User ID: ${user.user_ID}`;
+
+        const userLoginDiv = document.createElement("div");
+        userLoginDiv.textContent = `User Login: ${user.user_login}`;
+
+        userDiv.appendChild(avatarImg);
+        userDiv.appendChild(userIdDiv);
+        userDiv.appendChild(userLoginDiv);
+
+        usersContainer.appendChild(userDiv);
+    });
+}
+const BASE_URL = 'https://stage-two.i-wp-dev.com/wp-json/v4/'
 // const data = {
 //     email: "mournexxx@gmail.com",
 //     password: "4fcMc463!"
 // }
-// const postUrl = "https://stage-two.i-wp-dev.com/wp-json/v4/registration";
-
+// const REGISTRATION_URL = BASE_URL + 'registration'
+// 
 // const postOptions = {
 //     method: "POST",
 //     headers: {
@@ -23,7 +47,7 @@
 //     });
 // jwt: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL3N0YWdlLXR3by5pLXdwLWRldi5jb20vIiwiYXVkIjoiaHR0cHM6Ly9zdGFnZS10d28uaS13cC1kZXYuY29tLyIsImlhdCI6MTM1Njk5OTUyNCwibmJmIjoxMzU3MDAwMDAwLCJkYXRhIjp7ImlkIjoxNiwidXNlcl9lbWFpbCI6Im1vdXJuZXh4eEBnbWFpbC5jb20iLCJ1c2VyX3Bhc3MiOiI0ZmNNYzQ2MyEifX0.mrr7x8hPrk7ZzTyup8vRiKLewsDss6ZGAY76fFbScr8', message: 'The User was created successfully'
 const jwtToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL3N0YWdlLXR3by5pLXdwLWRldi5jb20vIiwiYXVkIjoiaHR0cHM6Ly9zdGFnZS10d28uaS13cC1kZXYuY29tLyIsImlhdCI6MTM1Njk5OTUyNCwibmJmIjoxMzU3MDAwMDAwLCJkYXRhIjp7ImlkIjoxNiwidXNlcl9lbWFpbCI6Im1vdXJuZXh4eEBnbWFpbC5jb20iLCJ1c2VyX3Bhc3MiOiI0ZmNNYzQ2MyEifX0.mrr7x8hPrk7ZzTyup8vRiKLewsDss6ZGAY76fFbScr8';
-const getUrl = "https://stage-two.i-wp-dev.com/wp-json/v4/users";
+const USERS_URL = BASE_URL + 'users'
 const getOptions = {
     method: "GET",
     headers: {
@@ -31,35 +55,15 @@ const getOptions = {
     }
 };
 
-fetch(getUrl, getOptions)
+fetch(USERS_URL, getOptions)
     .then(response => response.json())
-    .then(data => {
-        const filteredUsers = data.filter(user => !user.first_name);
-        filteredUsers.forEach(user => {
-            const userDiv = document.createElement("div");
-            userDiv.className = "user";
+    .then(data => createDivElements(data))
+    .catch(error => console.log(error))
 
-            const avatarImg = document.createElement("img");
-            avatarImg.src = user.avatar_url;
-            avatarImg.alt = "Avatar";
 
-            const userIdDiv = document.createElement("div");
-            userIdDiv.textContent = `User ID: ${user.user_ID}`;
-
-            const userLoginDiv = document.createElement("div");
-            userLoginDiv.textContent = `User Login: ${user.user_login}`;
-
-            userDiv.appendChild(avatarImg);
-            userDiv.appendChild(userIdDiv);
-            userDiv.appendChild(userLoginDiv);
-
-            usersContainer.appendChild(userDiv);
-        });
-    }
-    )
-const putUrl = "https://stage-two.i-wp-dev.com/wp-json/v4/user";
+const USER_URL = BASE_URL + 'user'
 const putData = {
-    id: 20,
+    id: 18,
     avatar_url: "https://cs5.pikabu.ru/images/big_size_comm/2015-04_2/14285721368204.jpg"
 };
 const putOptions = {
@@ -71,8 +75,7 @@ const putOptions = {
     body: JSON.stringify(putData)
 };
 
-fetch(putUrl, putOptions)
-
+fetch(USER_URL, putOptions)
 
 
 
